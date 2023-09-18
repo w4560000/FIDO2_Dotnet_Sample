d2c25efd0364e64188f5b3dff94ae45fd3188c41
w4560000 <39783609+w4560000@users.noreply.github.com>
2023-09-18 17:38:01 +0800

    加入專案檔案。

---
+++ FIDO2_Dotnet_Sample/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FIDO2_Dotnet_Sample.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
